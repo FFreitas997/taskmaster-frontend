3526e8914ee53da21188aeef29e4d1b0b17811e2
FFreitas <francisco.freitas.ff@gmail.com>
2024-07-22 21:02:23 +0100

    login with keycloak

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {KeycloakService} from "../../services/keycloak/keycloak.service";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  constructor(private keycloakService: KeycloakService) {}
+
+  async ngOnInit() {
+    await this.keycloakService.login();
+  }
 }
